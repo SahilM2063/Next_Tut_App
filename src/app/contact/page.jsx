a9b7a0b3bc0b82d7ac13840a388f7065282695ac
Sahil Makadia <makadiasahil9846@gmail.com>
2024-02-10 19:15:47 +0530

    Contact Page Created With UI

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,28 @@
+import styles from "./contact.module.css";
+import Image from "next/image";
+
 const ContactPage = () => {
-  return <h1>ContactPage</h1>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image
+          src="/contact.png"
+          alt="contact"
+          fill
+          className={styles.contactImage}
+        />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="#" className={styles.form}>
+          <input type="text" placeholder="Name and Surname" />
+          <input type="text" placeholder="Email Address" />
+          <input type="text" placeholder="Phone Number (Optional)" />
+          <textarea cols="30" rows="10" placeholder="Message"></textarea>
+          <button className={styles.formButton}>Send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;

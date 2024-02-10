91f4d6f9717e5154adeb2f700c720512b65eba44
Sahil Makadia <makadiasahil9846@gmail.com>
2024-02-10 18:54:58 +0530

    About Page Created With UI

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,8 +3,41 @@ import styles from "./about.module.css";
 
 const AboutPage = () => {
   return (
-    <div className={styles.imgContainer}>
-      <Image src="/about.png" alt="about" fill />
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subTitle}>About Agency</h2>
+        <h1 className={styles.title}>
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h1>
+        <p className={styles.desc}>
+          We create digital ideas that are bigger, bolder, braver and better. We
+          believe in good ideas flexibility and precission We're world's our
+          special team best consulting & finance provider. Wide range of web and
+          software development services.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1 className={styles.boxTitle}>10 K+</h1>
+            <p className={styles.boxDesc}>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1 className={styles.boxTitle}>10 K+</h1>
+            <p className={styles.boxDesc}>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1 className={styles.boxTitle}>10 K+</h1>
+            <p className={styles.boxDesc}>Year of experience</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image
+          src="/about.png"
+          alt="about"
+          fill
+          className={styles.aboutImage}
+        />
+      </div>
     </div>
   );
 };
